01bd5a25eb3a0bd11437169279676a6809a686a7
mongwell <19lion@gmail.com>
2017-12-10 20:57:25 -0600

    fixed bug with core usage value obtainment

--- obtain_raw_data.sh
+++ obtain_raw_data.sh
@@ -4,7 +4,7 @@
 #core usage percentage by usr and ni, then sys
 for i in {0..7}
 do
-    top -bn 1 | grep %Cpu$i | awk '{printf substr($3, 1, length($3)-4)}END{printf ","}END{printf substr($3, 5)}END{printf ";"}' >> py-input.txt
+    top -bn 1 | grep %Cpu$i | awk '{printf substr($3, 1, length($3)-4)}END{printf ","}END{printf substr($3, 6)}END{printf ";"}' >> py-input.txt
 done
 
 printf "\n" >> py-input.txt
@@ -19,3 +19,5 @@ printf "\n" >> py-input.txt
 
 #RAM usage as used/total
 free | grep Mem | awk '{printf substr($3, 1, length($3)-1)}END{printf "/"}END{print substr($2, 0, length($2)-1)}' >> py-input.txt
+
+cat py-input.txt
